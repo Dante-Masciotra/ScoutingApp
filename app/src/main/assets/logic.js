b7ce38849296c026a65208abe6a45f61b1e1f53d
Unknown <jazzy.d1302@outlook.com>
2019-03-23 20:16:32 -0400

    logic fixes

--- app/src/main/assets/logic.js
+++ app/src/main/assets/logic.js
@@ -2,12 +2,18 @@ var teamNumber;
 var matchNumber;
 var position;
 var keyNum = [];
+var test;
 function ComStart(){
     teamNumber = document.getElementById("teamNum").value;
     matchNumber = document.getElementById("matchNum").value;
 
     localStorage.setItem("Team Number"+(localStorage.length/2), teamNumber)
     localStorage.setItem("Match Number"+((localStorage.length-1)/2), matchNumber)
+
+}
+
+function getInfo(Num){
+    localStorage.setItem("clicked", Num.id);
 }
 
 function load(){
@@ -16,11 +22,16 @@ function load(){
    keyNum.push((localStorage.length/2)-(i+1));
 console.log[i]
    var nbutton = document.createElement("button")
-   nbutton.setAttribute("id","new");
+   nbutton.setAttribute("id","Team Number"+keyNum[i]);
+   nbutton.setAttribute("onclick","getInfo(this)")
    var a = document.createElement("a")
    a.setAttribute("href","info.html");
    a.innerText= " Team: "+localStorage.getItem("Team Number"+keyNum[i]) +" Match: "+localStorage.getItem("Match Number"+keyNum[i]);
   document.body.appendChild(nbutton)
   nbutton.appendChild(a)
    }
+   function loadInfo(){
+       var test
+   }
+
 }
