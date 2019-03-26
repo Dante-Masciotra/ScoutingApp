82898498c47ec33c6d455ae572c904fcc3cd2774
Unknown <jazzy.d1302@outlook.com>
2019-03-26 00:16:34 -0400

    fixed the api problem `\(^0^)/`

--- app/src/main/java/com/example/scout/MainActivity.java
+++ app/src/main/java/com/example/scout/MainActivity.java
@@ -3,14 +3,15 @@ package com.example.scout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         WebView view = new WebView(this);
+        view.setWebViewClient(new WebViewClient());
         view.getSettings().setJavaScriptEnabled(true);
         view.loadUrl("file:///android_asset/index.html");
         view.getSettings().setDomStorageEnabled(true);
